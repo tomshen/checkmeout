73c180f08a77afff447b5347ca9c200cd27ebed2
Tom Shen <tom@tomshen.me>
2013-10-06 07:54:35 +0000

    Working app flow

--- static/js/app.js
+++ static/js/app.js
@@ -62,8 +62,8 @@ function verify(items) {
     total: intToPrice(total)
   }));
 
-  $('.receipt-action').click(function () {
-    var ri = $('.receipt-item');
+  $('#verify .receipt-action').click(function () {
+    var ri = $('#verify .receipt-item');
     var total = 0;
     items = _.map(ri.slice(0, ri.length - 1), function (itemNode) {
       var i = {};
@@ -72,35 +72,124 @@ function verify(items) {
       total += i.price;
       return i;
     });
+    $('#verify').addClass('is-hidden');
     friends(items, total);
   });
 }
 
 function friends(items, total) {
-  $('#verify').addClass('is-hidden');
   $('#friends').removeClass('is-hidden');
 
   venmo.friends(function (friends) {
     var friendsTemplate = _.template($('#template-venmo-friends').html());
-    console.log(friends);
     $('#friends').html(friendsTemplate({ venmoFriends: friends }));
     $('.friend').click(function () {
       $(this).toggleClass('is-selected');
     });
 
-    $('.receipt-action').click(function () {
+    $('#friends .receipt-action').click(function () {
       var selectedFriends = _.map($('.friend.is-selected'), function(e) {
         $(e).data();
-        return $.data(e, 'userId')
+        return $.data(e, 'userId');
       });
-      claim(selectedFriends, items, total);
+      $('#friends').addClass('is-hidden');
+      claim(friends, selectedFriends, items, total);
     });
   });
 }
 
-function claim(selectedFriends, items, total) {
-  $('#friends').addClass('is-hidden');
+function claim(friends, selectedFriends, items, fullTotal) {
   $('#claim').removeClass('is-hidden');
+
+  var claimTotals = [];
+  var itemFriends = {};
+
+  var claimTemplate = _.template($('#template-claim').html());
+
+  function chooseClaims(i) {
+    var friend = _.filter(friends, function (f) {
+      return f.id === selectedFriends[i];
+    })[0];
+
+    var currentUser = friend.display_name;
+
+    $('#claim').html(claimTemplate({
+      currentUser: currentUser,
+      items: convertItems(JSON.parse(JSON.stringify(items))),
+      total: '$0.00'
+    }));
+
+    $('#claim .receipt-item').click(function () {
+      $(this).toggleClass('claimed');
+      var totalPriceNode = $('#claim .receipt-item.total .item-cost');
+      if($(this).hasClass('claimed'))
+        totalPriceNode.html(intToPrice(priceToInt(totalPriceNode.html())
+                                     + priceToInt($(this).children('.item-cost')[0].innerHTML)));
+      else
+        totalPriceNode.html(intToPrice(priceToInt(totalPriceNode.html())
+                                     - priceToInt($(this).children('.item-cost')[0].innerHTML)));
+    });
+
+    $('#claim .receipt-action').click(function () {
+      var total = 0;
+      var claimedItems = _.map($('.receipt-item.claimed'), function (itemNode, i) {
+        var i = {};
+        i.name = itemNode.childNodes[1].innerHTML.trim();
+        i.price = priceToInt(itemNode.childNodes[3].innerHTML);
+
+        total += i.price;
+
+        if (itemFriends.hasOwnProperty(i))
+          itemFriends[i].push(currentUser);
+        else itemFriends[i] = [ currentUser ];
+
+        return i;
+      });
+      claimTotals.push({
+        name: currentUser,
+        price: total,
+        userId: selectedFriends[i],
+        items: claimedItems
+      });
+      if (i === selectedFriends.length - 1) {
+        $('#claim').addClass('is-hidden');
+        checkTotal(fullTotal, claimTotals, itemFriends);
+      } else chooseClaims(i + 1);
+    });
+  }
+  chooseClaims(0);
+}
+
+function checkTotal(total, claimTotals, itemFriends) {
+  $('#total').removeClass('is-hidden');
+  var totalTemplate = _.template($('#template-total').html());
+
+  var remainingTotal = total;
+
+  _.each(claimTotals, function (claim) {
+    remainingTotal -= claim.price;
+  });
+
+  $('#total').html(totalTemplate({
+    total: intToPrice(total),
+    claimTotals: convertItems(claimTotals),
+    remainingTotal: intToPrice(remainingTotal)
+  }));
+
+  $('#total .receipt-action').click(function () {
+    $('#total').addClass('is-hidden');
+    confirm(claimTotals);
+  });
+}
+
+function confirm(claimTotals) {
+  $('#confirm').removeClass('is-hidden');
+  _.each(claimTotals, function (claim) {
+    var note = JSON.stringify(_.pluck(claim.items, 'name'));
+    venmo.pay(function (res) {
+      console.log(res);
+    }, claim.userId, note.slice(1, note.length - 1), '-' + claim.price.slice(1));
+  });
 }
 
 signin();
