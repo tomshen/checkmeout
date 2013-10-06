7d0b6883d4b10e70539fff032e10584fc226f4b9
Tom Shen <tom@tomshen.me>
2013-10-06 03:25:20 +0000

    Add receipt verify interface

--- static/js/app.js
+++ static/js/app.js
@@ -28,8 +28,42 @@ function picture() {
   });
 }
 
-function verify(receipt) {
+function convertPrice(price) {
+  var p = price.toString(10);
+  if (p.length === 0)
+    return '$0.00';
+  if (p.length === 1)
+    return '$0.0' + p;
+  if (p.length === 2)
+    return '$0.' + p;
+  return '$' + p.slice(0, p.length - 2) + '.' + p.slice(p.length - 2);
+}
+
+function convertItems(items) {
+  return _.map(items, function (item) {
+    item.price = convertPrice(item.price);
+    return item;
+  })
+}
+
+function verify(items) {
   $('#verify').removeClass('is-hidden');
+  var total = _.reduce(items, function (memo, item) {
+    return memo + item.price;
+  }, 0);
+
+  var verifyTemplate = _.template($('#template-verify').html());
+  $('#verify').html(verifyTemplate({
+    items: convertItems(items),
+    total: convertPrice(total)
+  }));
+
+  friends(items, total);
+}
+
+function friends(items, total) {
+  $('#verify').addClass('is-hidden');
+  $('#friends').addClass('is-hidden');
 }
 
 signin();
