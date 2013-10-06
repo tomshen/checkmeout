345d8ac95887aeb6e36feb299d23dcdd2246bb47
Tom Shen <tom@tomshen.me>
2013-10-06 02:24:27 +0000

    Add signin and upload interface

---
+++ static/js/app.js
@@ -0,0 +1,35 @@
+var venmo = Venmo(1424);
+
+function signin() {
+  if (venmo.isAuthorized()) {
+    $('#signin').addClass('is-hidden');
+    picture();
+  }
+
+  $('.signin-button').click(function () {
+    venmo.authorize();
+  });
+}
+
+function picture() {
+  $('#picture').removeClass('is-hidden');
+  $('.signin-submit').click(function () {
+    $.ajax({
+      url: '/upload',
+      data: new FormData($('#image-upload')[0]),
+      processData: false,
+      contentType: false,
+      type: 'POST',
+      success: function (receipt) {
+        $('#picture').addClass('is-hidden');
+      }
+    });
+  });
+}
+
+function verify(receipt) {
+  $('#verify').removeClass('is-hidden');
+  console.log(receipt);
+}
+
+signin();
